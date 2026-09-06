bde74e33609924b99b51502d6e51fa303fd1787d
S-A-N-J-U <sanju54901@gmail.com>
2026-09-06 21:38:12 +0530

    posts

--- src/pages/AllPosts.jsx
+++ src/pages/AllPosts.jsx
@@ -1,41 +1,3 @@
-// import React, {useState, useEffect} from 'react'
-// import { Container, PostCard } from '../components'
-// import appwriteService from "../appwrite/config";
-
-// function AllPosts() {
-//     const [posts, setPosts] = useState([])
-//     useEffect(() => {}, [])
-//     appwriteService.getPosts([]).then((posts) => {
-//         if (posts) {
-//             setPosts(posts.documents)
-//         }
-//     })
-//   return (
-//     <div className='w-full py-8'>
-//         <Container>
-//             <div className='flex flex-wrap'>
-//                 {posts.map((post) => (
-//                     <div key={post.$id} className='p-2 w-1/4'>
-//                         <PostCard {...post} />
-//                     </div>
-//                 ))}
-//             </div>
-//             </Container>
-//     </div>
-//   )
-// }
-
-// export default AllPosts
-
-
-
-
-
-
-
-
-
-
 import React, { useState, useEffect } from 'react'
 import { Container, PostCard } from '../components'
 import appwriteService from "../appwrite/config";
@@ -49,7 +11,7 @@ function AllPosts() {
                 setPosts(posts.documents)
             }
         })
-    }, []) // Runs once when the component mounts
+    }, []) 
 
     return (
         <div className='w-full py-8'>
