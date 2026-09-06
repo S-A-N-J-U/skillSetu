be3e8bacfe042a79a12dcf33768d1070dc09b655
S-A-N-J-U <sanju54901@gmail.com>
2026-09-06 17:37:17 +0530

    RTE

--- src/components/RTE/RTE.jsx
+++ src/components/RTE/RTE.jsx
@@ -14,7 +14,7 @@ export default function RTE({name, control, label, defaultValue =""}) {
     control={control}
     render={({field: {onChange}}) => (
         <Editor
-         apiKey= 'csfn10mtebsldge3smkruv2neemhuvzs794f7yirdy6gwwny'
+          apiKey='csfn10mtebsldge3smkruv2neemhuvzs794f7yirdy6gwwny'
         initialValue={defaultValue}
         init={{
             initialValue: defaultValue,
